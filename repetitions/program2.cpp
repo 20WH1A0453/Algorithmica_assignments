37b551a139670b9685e4b1b809e42933b36c41a2
20WH1A0453 <119663664+20WH1A0453@users.noreply.github.com>
2022-12-11 18:43:04 +0530

    add
    add

--- repetitions/program2.cpp
+++ repetitions/program2.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main(){
 	string s;
 	cin>>s;
-	char ch;
 	int vowel=0,consonant=0;
 	for(int i=0;i<=s.length();i++){
 		if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
