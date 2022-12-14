a7b169c4943a247999db614df491777c552fc8be
20WH1A0453 <119663664+20WH1A0453@users.noreply.github.com>
2022-12-14 22:51:11 +0530

    add
    add

---
+++ nested loops/program9.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int row,col;
+	for(col=row+1;col<=10;col++) cout<<"\t"<<col<<" ";
+		cout<<"\n";
+	for(row=1;row<=10;row++){
+		cout<<row;
+		for(col=1;col<=10;col++) cout<<"\t"<<row*col;
+		cout<<"\n";
+	}
+}
+
+
