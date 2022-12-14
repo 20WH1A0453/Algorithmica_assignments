be0574cb9c414e915684ef4434ab2c4d5cfc1619
20WH1A0453 <20wh1a0453@bvrithyderabad.edu.in>
2022-12-14 22:47:34 +0530

    add
    add

--- nested loops/program9.cpp
+++ nested loops/program9.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	int i,j;
-	for(j=i+1;j<=10;j++) cout<<"\t"<<j<<" ";
+	int row,col;
+	for(col=row+1;col<=10;col++) cout<<"\t"<<col<<" ";
 		cout<<"\n";
-	for(i=1;i<=10;i++){
-		cout<<i;
-		for(j=1;j<=10;j++) cout<<"\t"<<i*j;
+	for(row=1;row<=10;row++){
+		cout<<row;
+		for(col=1;col<=10;col++) cout<<"\t"<<row*col;
 		cout<<"\n";
 	}
 }
