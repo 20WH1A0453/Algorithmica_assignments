d71cca364527546d6f4c4d56924eb7b05a85828e
20WH1A0453 <20wh1a0453@bvrithyderabad.edu.in>
2022-12-21 20:49:17 +0530

    add
    add

---
+++ functions2/prog3.py
@@ -0,0 +1,31 @@
+#decimal to any radix
+def base10_to_Arbitrary(num):
+    for i in range(1,num):
+        num=num//i
+        print(num%base)
+
+    #if(num>=1):
+        #base10_to_Arbitrary(num // base)
+        #print(num%base,end=' ')
+
+
+#any radix to decimal
+def arbitrary_to_base10(num):
+    sum=0
+    for i in range(0,num):
+        rem=num%10
+        sum=sum+rem*(pow(base,i))
+        num=num//10
+    return sum
+
+def convertBase(num):
+    if (base >= 2 and base <= 16):
+        print(base10_to_Arbitrary(num))
+        print(arbitrary_to_base10(num))
+    else:
+        print('error')
+
+num=int(input('Enter number'))
+base=int(input('Enter base'))
+convertBase(num)
+print()
