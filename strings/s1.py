f6fe3b3fce2bbf251cc4f6c5bbdabe6fd3901dd2
20WH1A0453 <20wh1a0453@bvrithyderabad.edu.in>
2022-12-23 21:41:10 +0530

    add
    add

--- strings/s1.py
+++ strings/s1.py
@@ -1,8 +1,8 @@
 def removeMysteryLength(str):
     for i in range(1,len(str)):
-        if(len(str)<=9):
+        if(len(str)<=9): #if length of string is one digit
             return str[0:len(str)-i]
-        elif(len(str)<=99):
+        elif(len(str)<=99): #if length of string is two digits
             return str[0:len(str)-i-1]
 str=input("string")
 print(removeMysteryLength(str))
