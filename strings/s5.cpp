921bbbd351a4ac48d28b82b1b56802142a3a1805
20WH1A0453 <20wh1a0453@bvrithyderabad.edu.in>
2022-12-28 10:50:14 +0530

    updated
    updated

--- strings/s5.cpp
+++ strings/s5.cpp
@@ -2,29 +2,33 @@
 #include<string>
 using namespace std;
 
-string encrpytWithCaesar(){
-	string res="",s;
-	int pos=0,shift;
-	cin>>s>>shift;
+string encrpytWithCaesar(string s, int shift){
+	string res="",temp="";
+	int pos=0;
 	for(int i=0;i<=s.length();i++){
 	    if(s[i]>='a' && s[i]<='z'){
 	    	pos=s[i]-'a';
 	    	pos=(pos+shift)%26;
-		    res='a'+pos;
-		    cout<<res;
+		    temp='a'+pos;
+		    res+=temp;
+		    cout<<temp;
 		}
 	    else if(s[i]>='A' && s[i]<='Z'){
 	    		pos=s[i]-'A';
 	    		pos=(pos+shift)%26;
-		    	res='A'+pos;
-		    	cout<<res;
+		    	temp='A'+pos;
+		    	res+=temp;
+		    	cout<<temp;
 		}
 		else cout<<s[i];
 	}
 }
 
 int main(){
-	cout<<encrpytWithCaesar();	
+	string s;
+	int shift;
+	cin>>s>>shift;
+	cout<<encrpytWithCaesar(s,shift);	
 	return 0;
 }
 
