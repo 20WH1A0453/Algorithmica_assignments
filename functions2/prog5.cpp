3460492ef706882b265f2fe5d8cf00e668259725
20WH1A0453 <20wh1a0453@bvrithyderabad.edu.in>
2022-12-30 15:15:29 +0530

    add
    add

---
+++ functions2/prog5.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+double approximatePI(int n){
+	double s=0,i,sign=1;
+	int d;
+	for(i=1,d=2;i<=n;i++,d+=2){
+		s+=1.0/(d*(d+1)*(d+2))*sign;
+		sign*=-1;
+	}
+	return (s*4)+3;
+}
+int main(){
+		int n;
+		cin>>n;
+		cout<<approximatePI(n);
+	}
+	
+
